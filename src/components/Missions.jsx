bb57e4bd5f5a3b104867249267abfbdde0f49cbe
Jamile <milli.santana2017@gmail.com>
2022-11-22 21:01:15 -0300

    fix: correções de lint e caminho do componente MissionCard

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import Title from './Title';
 import missions from '../data/missions';
-import MissionCard from '../components/MissionCard'
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
-        { missions.map((mission) => (<MissionCard 
-        name={ mission.name }
-        key={ mission.name }
-        country={ mission.country }
-        destination={ mission.destination }
+        { missions.map((mission) => (<MissionCard
+          name={ mission.name }
+          key={ mission.name }
+          country={ mission.country }
+          destination={ mission.destination }
         />)) }
       </div>
     );
